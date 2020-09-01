e54f1f4cc4db79307462bd32ad7af6527f0053c3
oroafrica <52736386+oroafrica@users.noreply.github.com>
2020-09-01 13:32:45 +0200

    create world

--- oaworld.js
+++ oaworld.js
@@ -1,1 +1,17 @@
 
+class World
+{
+   constructor()
+  {
+      this.tgtOne = null;
+      this.tgtTwo = null;
+      this.handlerOne = null;
+      this.handlerTwo = null;
+  }
+  
+  render()
+  {
+    console.log("new world loaded!");
+  }
+}
+  
