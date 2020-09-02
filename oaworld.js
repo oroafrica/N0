a0519168f428bb377d9bb984882c7d07aa55b2e3
oroafrica <52736386+oroafrica@users.noreply.github.com>
2020-09-02 07:44:55 +0200

    sel-06

--- oaworld.js
+++ oaworld.js
@@ -27,13 +27,15 @@ class World
    }
   alloySel()
   {
-     //$(this.handlerOne).on("change",()=> {$(".ty-price").prepend("<span class='ty-price-num' style='font-size:0.6rem' >From </span>");});
-     //$(document).on("change",".ty-alloy",()=> {$(".ty-price").prepend("<span class='ty-price-num' style='font-size:0.6rem' >From </span>");});
-     $(document).bind("change",this.handlerOne,()=> {$(".ty-price").prepend("<span class='ty-price-num' style='font-size:0.6rem' >From </span>");});
+     $(document).bind("change",this.handlerOne,()=> 
+     {
+        $(". tmp-hold").remove();
+        $(".ty-price").prepend("<span class='ty-price-num' style='font-size:0.6rem' >From </span>");
+     });
   }
   getPrefix()
   {
-     $(".ty-price").prepend("<span class='ty-price-num' style='font-size:0.6rem' >From </span>");
+     $(".ty-price").prepend("<span class='ty-price-num tmp-hold' style='font-size:0.6rem' >From </span>");
   }
   render()
   {
