ea0bea212403d464b8ee1bda95149469b8a0032a
oroafrica <52736386+oroafrica@users.noreply.github.com>
2020-09-02 07:37:37 +0200

    sel-06

--- oaworld.js
+++ oaworld.js
@@ -28,7 +28,7 @@ class World
   alloySel()
   {
      //$(this.handlerOne).on("change",()=> {$(".ty-price").prepend("<span class='ty-price-num' style='font-size:0.6rem' >From </span>");});
-     $(document).on("change",".ty-alloy",()=> {console.log(this.selectedIndex);});
+     $(document).on("change",".ty-alloy",()=> {$(".ty-price").prepend("<span class='ty-price-num' style='font-size:0.6rem' >From </span>");});
   }
   getPrefix()
   {
