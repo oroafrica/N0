39b81f75ba9b10e73ec8b17a57eb47b4e93ce838
oroafrica <52736386+oroafrica@users.noreply.github.com>
2020-09-02 07:17:30 +0200

    selector listener

--- oaworld.js
+++ oaworld.js
@@ -9,15 +9,28 @@ class World
       this.handlerTwo = null;
   }
   
+   getAlloySel()
+   {
+      var tgt = $('select').toArray();
+      $.each(tgt, (index)=> 
+      {
+         if($(tgt).eq(index).find("option").length > 2 && $(tgt)[index].options[1].text === "Silver")
+         {
+            this.handlerOne =  $(tgt)[index];
+            console.log($(tgt)[index].prop('selectedIndex'));
+         }	
+      });
+   }
+   
   getPrefix()
   {
      $(".ty-price").prepend("<span class='ty-price-num' style='font-size:0.6rem' >From </span>");
-     
   }
   render()
   {
+      this.getAlloySel();
       this.getPrefix();
-      console.log("new world loaded!");
+      //console.log("new world loaded!");
   }
 }
   
